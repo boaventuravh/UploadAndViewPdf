d58ec596d0046951eeb64b3d1ee4971f11dbb595
boaventuravh <boaventurads.vitorhugo@gmail.com>
2023-05-03 15:17:30 -0300

    Add project files.

---
+++ UploadAndViewPdf/Controllers/FileController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UploadAndViewPdf.Models;
+
+namespace UploadAndViewPdf.Controllers
+{
+    public class FileController : Controller
+    {
+        IHostingEnvironment _hostingEnvironment = null;
+
+        public FileController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet]
+        public IActionResult Index(string fileName="")
+        {
+            FilesModel fileObj = new FilesModel();
+            fileObj.Name = fileName;
+
+            string path = $"{_hostingEnvironment.WebRootPath}\\files\\";
+            int nId = 1;
+
+            foreach(string pdfPath in Directory.EnumerateFiles(path, "*.pdf"))
+            {
+                fileObj.Files.Add(new FilesModel()
+                { 
+                    FileId = nId++,
+                    Name = Path.GetFileName(pdfPath),
+                    Path = pdfPath
+                });
+            }
+
+            return View(fileObj);
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile file, [FromServices] IHostingEnvironment hostingEnvironment)
+        {
+            string fileName = $"{hostingEnvironment.WebRootPath}\\files\\{file.FileName}";
+            using (FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+            return Index();
+        }
+
+        public IActionResult PdfViewerNewTab(string fileName) 
+        {
+            string path = _hostingEnvironment.WebRootPath+"\\files\\"+fileName;
+            return File(System.IO.File.ReadAllBytes(path), "application/pdf");
+        }
+    }
+}
---
+++ UploadAndViewPdf/Models/FilesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadAndViewPdf.Models
+{
+    public class FilesModel
+    {
+        public int FileId { get; set; } = 0;
+
+        public string Name { get; set; } = "";
+
+        public string Path { get; set; } = "";
+
+        public List<FilesModel> Files { get; set; } = new List<FilesModel>();
+
+    }
+}
